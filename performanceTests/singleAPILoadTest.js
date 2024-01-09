b7c424e2a8fa969e56d7ce79d0056b3e5b060aeb
gaurav.bharadwaj <gaurav.bharadwaj@zinc.systems>
2024-01-09 16:41:57 +0530

    load-testing-ci-fix-5

--- performanceTests/singleAPILoadTest.js
+++ performanceTests/singleAPILoadTest.js
@@ -9,9 +9,9 @@ const password = 'password';
 export let options = {
     vus: 100,
     stages: [
-        { duration: "10s", target: 10 },
-        { duration: "10s", target: 100 },
-        { duration: "10s", target: 0 }
+        { duration: "10s", target: 10},
+        { duration: "10s", target: 100},
+        { duration: "10s", target: 0}
     ],
     thresholds: {
         http_req_failed: ['rate<0.01'], // http errors should be less than 1%
@@ -31,3 +31,4 @@ export default function() {
         "status is 200": (r) => r.status === 200,
     });
 }
+
